5fb49a6caccc7c7eff7fbf9198113686ba99047b
James Quinn <jameson71@gmail.com>
2020-06-29 12:15:12 -0400

    This was the try with WELD for CDI which seems required for JSF 2.3 on Tomcat.

---
+++ src/utils/JSF23Activator.java
@@ -0,0 +1,11 @@
+package utils;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+@FacesConfig(version = JSF_2_3)
+@ApplicationScoped
+
+public class JSF23Activator {
+
+}
